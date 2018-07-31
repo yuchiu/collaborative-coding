1233b92c06fdc0cb0a217cbd0ab7ea8e77df8a8b
yuchiu <yuchiu2002@hotmail.com>
2018-07-31 09:49:56 -0400

    display data inside problem detail component

--- client/src/app/components/problem-detail/problem-detail.component.ts
+++ client/src/app/components/problem-detail/problem-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Problem } from "../../models/problem.model";
+import { ActivatedRoute, Params } from "@angular/router";
+import { inject } from "@angular/core/testing";
 
 @Component({
-  selector: 'app-problem-detail',
-  templateUrl: './problem-detail.component.html',
-  styleUrls: ['./problem-detail.component.css']
+  selector: "app-problem-detail",
+  templateUrl: "./problem-detail.component.html",
+  styleUrls: ["./problem-detail.component.css"]
 })
 export class ProblemDetailComponent implements OnInit {
-
-  constructor() { }
+  problem: Problem;
+  constructor(private route: ActivatedRoute, @Inject("data") private data) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.problem = this.data.getProblem(+params["id"]);
+    });
   }
-
 }
